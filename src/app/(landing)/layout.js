65e353372a709bc176ac889ed2fb44810d8828a4
Deni-Hd21 <131957825+Deni-Hd21@users.noreply.github.com>
2026-04-29 00:29:49 +0700

    Update layout.js
    add WhatsApp Floating at src/app/(landing)/layout.js

--- src/app/(landing)/layout.js
+++ src/app/(landing)/layout.js
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar";
+import WhatsAppFloat from "../components/WhatsAppFloat";
 
 export default function LandingLayout({ children }) {
   return (
     <>
       <Navbar />
       {children}
+      <WhatsAppFloat />
     </>
   );
 }
