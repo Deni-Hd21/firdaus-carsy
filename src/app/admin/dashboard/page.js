9b40d6482fb9c7745e527b41d242ef9f0750d003
Deni-Hd21 <131957825+Deni-Hd21@users.noreply.github.com>
2026-05-06 17:27:19 +0700

    perbaikan tipo dashboard admin

--- src/app/admin/dashboard/page.js
+++ src/app/admin/dashboard/page.js
@@ -42,7 +42,7 @@ export default function Dashboard() {
           <div className="flex justify-between items-center mb-8">
             <div>
               <h1 className="text-2xl font-bold text-white">Dashboard Admin</h1>
-              <p className="text-gray-400 text-sm mt-1">Firdaus Cars Syari'ah</p>
+              <p className="text-gray-400 text-sm mt-1">Firdaus Carsy</p>
             </div>
             <div className="flex items-center gap-2">
               <button onClick={() => router.push("/admin/pengaturan")} className="bg-slate-700 hover:bg-slate-600 text-white text-sm font-semibold px-4 py-2 rounded-xl transition">
