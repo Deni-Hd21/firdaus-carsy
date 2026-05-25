b0d4970910bc831c3352595c3db149f7360374f5
Deni-Hd21 <131957825+Deni-Hd21@users.noreply.github.com>
2026-05-25 12:39:24 +0700

    Remove unused URLs from sitemap
    Removed several URLs from the sitemap configuration.

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -6,29 +6,5 @@ export default function sitemap() {
       changeFrequency: "daily",
       priority: 1,
     },
-    {
-      url: "https://firdauscarsy.id/#tentang",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://firdauscarsy.id/#testimoni",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.7,
-    },
-    {
-      url: "https://firdauscarsy.id/#kontak",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.6,
-    },
-    {
-      url: "https://firdauscarsy.id/#faq",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.6,
-    },
   ];
 }
