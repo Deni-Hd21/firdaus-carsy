4c8069e5d283e850bf8648c7d8923c6178e4a7c9
Deni-Hd21 <a.deni.hidayat21.com>
2026-08-11 23:47:57 +0700

    perbaikan error local di laptop saat awal buka/pindah

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,25 @@
-headers: async () => [
-  {
-    source: "/(.*)\\.(ico|jpg|jpeg|png|gif|webp|svg|woff|woff2|ttf|otf|js|css)",
-    headers: [
-      {
-        key: "Cache-Control",
-        value: "public, max-age=31536000, immutable",
-      },
-    ],
-  },
-  {
-    source: "/(.*)",
-    headers: [
-      {
-        key: "Cache-Control",
-        value: "public, max-age=0, must-revalidate",
-      },
-    ],
-  },
-];
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  headers: async () => [
+    {
+      source: "/(.*)\\.(ico|jpg|jpeg|png|gif|webp|svg|woff|woff2|ttf|otf|js|css)",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "public, max-age=31536000, immutable",
+        },
+      ],
+    },
+    {
+      source: "/(.*)",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "public, max-age=0, must-revalidate",
+        },
+      ],
+    },
+  ],
+};
+
+export default nextConfig;
